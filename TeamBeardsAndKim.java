0651864e79661bcfcb29c5a6b91a5398cdd74614
schirripad@moravian.edu <schirripad@moravian.edu>
2019-02-04 12:25:34 -0500

    Added Team File

---
+++ TeamBeardsAndKim.java
@@ -0,0 +1,13 @@
+package Project00;
+
+public class TeamBeardsAndKim extends Team{
+
+	public TeamBeardsAndKim(String name) {
+		super(name);
+	}
+
+	@Override
+	public void createTeamMembers() {		
+	}
+
+}
